6e0ca7bd2ff475e834ef67c303e573fae0acb4a7
abhineet08 <abhineet.agarwal@practo.com>
2015-08-20 12:23:55 +0000

    adding new sniffs

--- fabpolish/contrib.py
+++ fabpolish/contrib.py
@@ -31,3 +31,82 @@ def find_pep8_violations():
         "grep -PZz '\.py$' | "
         "xargs -0 pep8"
     )
+
+    
+@sniff(severity='major', timing='fast')
+def fix_file_permission():
+    """Fixing permissions for files"""
+    info('Fixing permissions for files')
+    return local(
+        "git ls-files -z | "
+        "grep -PvZz '\.sh$' | "
+        "xargs -0 chmod -c 0664 > /dev/null 2>&1"
+    )
+
+    
+@sniff(severity='major', timing='fast')
+def fix_script_permission():
+    # Fix script permissions
+    info('Fixing script permissions...')
+    return local(
+        "git ls-files -z | "
+        "grep -PZz '\.sh$' | "
+        "xargs -0 -r chmod 0775 >/dev/null 2>&1"
+    )
+
+    
+@sniff(severity='major', timing='fast')
+def fix_white_space():
+    info('Fixing whitespace errors...')
+    return local(
+        "git ls-files -z | "
+        "grep -PZvz '\.(ico|jpg|png|gif|eot|ttf|woff|wav)$' | "
+        "xargs -0 grep -PlZn '(\\s+$)|(\\t)' | "
+        "tee /dev/stderr | "
+        "xargs -0 -r sed -i -e 's/\\s\\+$//' "
+    )
+
+
+@sniff(severity='major', timing='fast')
+def convert_tab_spaces():
+    info('Converting tab to spaces...')
+    return local(
+        "git ls-files -z | "
+        "grep -PZvz '\.(ico|jpg|png|gif|eot|ttf|woff|wav)$' | "
+        "xargs -0 grep -PlZn '(\\s+$)|(\\t)' | "
+        "tee /dev/stderr | "
+        "xargs -0 -r sed -i -e 's/\\t/    /g' "
+    )
+
+
+@sniff(severity='critical', timing='fast')
+def check_migration_branch():
+    """Checking migration branches"""
+    info('Checking migration branches...')
+    return local("! alembic branches | grep branchpoint")
+
+
+@sniff(severity='major', timing='fast')
+def remove_debug_info():
+    """Check and remove debugging print statements"""
+    info('Checking for debug print statements')
+    return local(
+        "! git ls-files -z | "
+        "grep -PZvz 'fabfile.py' | "
+        "grep -PZz \.py$ | "
+        "xargs -0 grep -Pn \'(?<![Bb]lue|>>> )print\' | "
+        "grep -v NOCHECK"
+    )
+
+
+@sniff(severity='major', timing='fast')
+def check_image_edited():
+    # Check if image files have been edited
+    info('Checking if image files have been edited...')
+    info('Explanation: A new image should be created when editing images to avoid browser caching')
+    branch = local('git rev-parse --abbrev-ref HEAD')
+    return local(
+        '! git diff master...' + branch +
+        ' --name-only --diff-filter=M | ' +
+        'grep ".gif\|.png\|.jpg"'
+    )
