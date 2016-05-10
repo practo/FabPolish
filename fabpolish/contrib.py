15b7707bee73aea591d096f2a148de17d1f37366
Deven Vyas <vyas.deven@gmail.com>
2016-05-10 14:39:38 +0530

    Removed `always exit with 0` change

--- fabpolish/contrib.py
+++ fabpolish/contrib.py
@@ -134,4 +134,4 @@ def composer_validate():
 @sniff(severity='major', timing='fast')
 def run_eslint():
     info('Running ESLint:')
-    return local("! ./node_modules/eslint/bin/eslint.js .")
+    return local("./node_modules/eslint/bin/eslint.js .")
