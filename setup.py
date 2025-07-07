a5e2767718ae8bd3ccd39881e21d8e9560d565da
JatsVangani <jamesvangani@gmail.com>
2025-07-07 16:56:32 +0530

    version updated

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='fab-polish',
-      version='1.2.0',
+      version='1.3.0',
       description='Polish git versioned source code using Fabric',
       url='https://github.com/practo/FabPolish',
       author='J Kishore Kumar',
